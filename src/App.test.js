d5d02a676fe199568133c396065b240ccddaad27
EugeneTepin <4godwork@gmail.com>
2019-09-09 12:11:52 +0300

    TodoFooter: added to app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import 'todomvc-app-css/index.css';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from 'reducers'
+import App from 'App';
+const store = createStore(todoApp);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+
+
+
+
